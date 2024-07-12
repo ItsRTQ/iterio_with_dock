87f4ea1436c2e25631c8248b8e497baf929151a0
itsrtq <titanbabrisa@gmail.com>
2024-07-12 01:50:41 -0400

    only using entry point

--- docker_functionality/entrypoint.sh
+++ docker_functionality/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+#install npm and node for tailwind
+apt-get update && apt-get install -y nodejs npm
+npm install
+
+
 # Install Tailwind CSS
 python manage.py tailwind install
 
@@ -7,8 +13,11 @@ python manage.py tailwind install
 python manage.py makemigrations
 python manage.py migrate
 
+
 # Start Tailwind for the beauty of iterio
-python manage.py tailwind start
+#python manage.py tailwind start
 
 # Start the Iterio app
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
+
+python3 manage.py tailwind start && python3 manage.py runserver 0.0.0.0:8000
